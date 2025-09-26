6c5b0b8235fda1f833e8de99e675e098f5df89f9
lenovo <2788510485@qq.com>
2025-09-26 20:05:58 +0800

    A4 partI && intro constantpropagation

--- A4/tai-e/src/main/java/pascal/taie/analysis/dataflow/analysis/constprop/ConstantPropagation.java
+++ A4/tai-e/src/main/java/pascal/taie/analysis/dataflow/analysis/constprop/ConstantPropagation.java
@@ -22,23 +22,19 @@
 
 package pascal.taie.analysis.dataflow.analysis.constprop;
 
+import fj.P;
 import pascal.taie.analysis.dataflow.analysis.AbstractDataflowAnalysis;
 import pascal.taie.analysis.graph.cfg.CFG;
 import pascal.taie.config.AnalysisConfig;
 import pascal.taie.ir.IR;
-import pascal.taie.ir.exp.ArithmeticExp;
-import pascal.taie.ir.exp.BinaryExp;
-import pascal.taie.ir.exp.BitwiseExp;
-import pascal.taie.ir.exp.ConditionExp;
-import pascal.taie.ir.exp.Exp;
-import pascal.taie.ir.exp.IntLiteral;
-import pascal.taie.ir.exp.ShiftExp;
-import pascal.taie.ir.exp.Var;
+import pascal.taie.ir.exp.*;
 import pascal.taie.ir.stmt.DefinitionStmt;
 import pascal.taie.ir.stmt.Stmt;
 import pascal.taie.language.type.PrimitiveType;
 import pascal.taie.language.type.Type;
 import pascal.taie.util.AnalysisException;
+
+import java.util.Optional;
 
 public class ConstantPropagation extends
         AbstractDataflowAnalysis<Stmt, CPFact> {
@@ -57,18 +53,34 @@ public class ConstantPropagation extends
     @Override
     public CPFact newBoundaryFact(CFG<Stmt> cfg) {
         // TODO - finish me
-        return null;
+        CPFact fact = new CPFact();
+        for (Var var : cfg.getIR().getVars()) {
+            if (canHoldInt(var)) fact.update(var, Value.getUndef());
+        }
+
+        for (Var var : cfg.getIR().getParams()) {
+            if (canHoldInt(var)) fact.update(var, Value.getNAC());
+        }
+
+        if (cfg.getIR().getThis() != null && canHoldInt(cfg.getIR().getThis())) fact.update(cfg.getIR().getThis(), Value.getNAC());
+        return fact;
     }
 
     @Override
     public CPFact newInitialFact() {
         // TODO - finish me
-        return null;
+        return new CPFact();
     }
 
     @Override
     public void meetInto(CPFact fact, CPFact target) {
         // TODO - finish me
+        for (Var var : fact.keySet()) {
+            Value v1 = fact.get(var);
+            Value v2 = target.get(var);
+            Value meetV = meetValue(v1, v2);
+            target.update(var, meetV);
+        }
     }
 
     /**
@@ -76,12 +88,31 @@ public class ConstantPropagation extends
      */
     public Value meetValue(Value v1, Value v2) {
         // TODO - finish me
-        return null;
+        if (v1.isNAC() || v2.isNAC()) return Value.getNAC();
+        else if (v1.isUndef() && v2.isConstant()) return v2;
+        else if (v1.isConstant() && v2.isUndef()) return v1;
+        else if (v1.isUndef() && v2.isUndef()) return Value.getUndef();
+        else if (v1.isConstant() && v2.isConstant() && v1.equals(v2)) return v1;
+        else return Value.getNAC();
     }
 
     @Override
     public boolean transferNode(Stmt stmt, CPFact in, CPFact out) {
         // TODO - finish me
+        if (stmt instanceof DefinitionStmt<?,?> definitionStmt) {
+            LValue left = definitionStmt.getLValue();
+            RValue right = definitionStmt.getRValue();
+
+            CPFact newOut = in.copy();
+
+            if (left instanceof Var var) {
+                if (canHoldInt(var)) {
+                    Value value = evaluate(right, in);
+                    newOut.update(var, value);
+                }
+                return out.copyFrom(newOut);
+            }
+        }
         return false;
     }
 
@@ -112,6 +143,100 @@ public class ConstantPropagation extends
      */
     public static Value evaluate(Exp exp, CPFact in) {
         // TODO - finish me
-        return null;
+        if (exp instanceof Var var) {
+            return in.get(var);
+        }
+        else if (exp instanceof IntLiteral intLiteral) {
+            return Value.makeConstant(intLiteral.getValue());
+        }
+        else if (exp instanceof BinaryExp binaryExp) {
+            Var var1 = binaryExp.getOperand1();
+            Var var2 = binaryExp.getOperand2();
+            Value v1 = in.get(var1);
+            Value v2 = in.get(var2);
+            if (v1.isUndef() || v2.isUndef()) return Value.getUndef();
+            else if (v1.isNAC() || v2.isNAC()) {
+                if (binaryExp instanceof ArithmeticExp arithmeticExp) {
+                    if (arithmeticExp.getOperator() == ArithmeticExp.Op.DIV || arithmeticExp.getOperator() == ArithmeticExp.Op.REM) {
+                        if (v2.isConstant() && v2.getConstant() == 0) return Value.getUndef();
+                    }
+                }
+                return Value.getNAC();
+            }
+            else {
+                if (binaryExp instanceof ArithmeticExp arithmeticExp) {
+                    return arithmeticEvaluate(arithmeticExp.getOperator(), v1, v2);
+                }
+                else if (binaryExp instanceof BitwiseExp bitwiseExp) {
+                    return bitwiseEvaluate(bitwiseExp.getOperator(), v1, v2);
+                }
+                else if (binaryExp instanceof ConditionExp conditionExp) {
+                    return conditionEvaluate(conditionExp.getOperator(), v1, v2);
+                }
+                else if (binaryExp instanceof ShiftExp shiftExp) {
+                    return shiftEvaluate(shiftExp.getOperator(), v1, v2);
+                }
+                else {
+                    throw new AnalysisException("Unknown binary expression: " + binaryExp);
+                }
+            }
+
+        }
+        else return Value.getNAC();
+    }
+
+    private static Value arithmeticEvaluate(ArithmeticExp.Op operator, Value v1, Value v2) {
+        int val1 = v1.getConstant();
+        int val2 = v2.getConstant();
+        return switch (operator) {
+            case ADD -> Value.makeConstant(val1 + val2);
+            case SUB -> Value.makeConstant(val1 - val2);
+            case MUL -> Value.makeConstant(val1 * val2);
+            case DIV -> {
+                if (val2 == 0) yield Value.getUndef();
+                yield Value.makeConstant(val1 / val2);
+            }
+            case REM -> {
+                if (val2 == 0) yield Value.getUndef();
+                yield Value.makeConstant(val1 % val2);
+            }
+            default -> throw new AnalysisException("Unknown arithmetic operator: " + operator);
+        };
+    }
+
+    private static Value bitwiseEvaluate(BitwiseExp.Op operator, Value v1, Value v2) {
+        int val1 = v1.getConstant();
+        int val2 = v2.getConstant();
+        return switch (operator) {
+            case AND -> Value.makeConstant(val1 & val2);
+            case OR -> Value.makeConstant(val1 | val2);
+            case XOR -> Value.makeConstant(val1 ^ val2);
+            default -> throw new AnalysisException("Unknown bitwise operator: " + operator);
+        };
+    }
+
+    private static Value conditionEvaluate(ConditionExp.Op operator, Value v1, Value v2) {
+        int val1 = v1.getConstant();
+        int val2 = v2.getConstant();
+        return switch (operator) {
+            case EQ -> Value.makeConstant(val1 == val2 ? 1 : 0);
+            case NE -> Value.makeConstant(val1 != val2 ? 1 : 0);
+            case LT -> Value.makeConstant(val1 < val2 ? 1 : 0);
+            case GT -> Value.makeConstant(val1 > val2 ? 1 : 0);
+            case LE -> Value.makeConstant(val1 <= val2 ? 1 : 0);
+            case GE -> Value.makeConstant(val1 >= val2 ? 1 : 0);
+            default -> throw new AnalysisException("Unknown condition operator: " + operator);
+        };
+    }
+
+    private static Value shiftEvaluate(ShiftExp.Op operator, Value v1, Value v2) {
+        int val1 = v1.getConstant();
+        int val2 = v2.getConstant();
+        return switch (operator) {
+            case SHL -> Value.makeConstant(val1 << val2);
+            case SHR -> Value.makeConstant(val1 >> val2);
+            case USHR -> Value.makeConstant(val1 >>> val2);
+            default -> throw new AnalysisException("Unknown shift operator: " + operator);
+        };
     }
 }
--- A4/tai-e/src/main/java/pascal/taie/analysis/graph/callgraph/CHABuilder.java
+++ A4/tai-e/src/main/java/pascal/taie/analysis/graph/callgraph/CHABuilder.java
@@ -23,14 +23,20 @@
 package pascal.taie.analysis.graph.callgraph;
 
 import pascal.taie.World;
+import pascal.taie.ir.exp.InvokeExp;
+import pascal.taie.ir.exp.InvokeInstanceExp;
 import pascal.taie.ir.proginfo.MethodRef;
 import pascal.taie.ir.stmt.Invoke;
+import pascal.taie.ir.stmt.Stmt;
 import pascal.taie.language.classes.ClassHierarchy;
 import pascal.taie.language.classes.JClass;
 import pascal.taie.language.classes.JMethod;
 import pascal.taie.language.classes.Subsignature;
+import polyglot.ast.Call;
 
+import java.lang.invoke.CallSite;
 import java.util.ArrayDeque;
+import java.util.HashSet;
 import java.util.Queue;
 import java.util.Set;
 
@@ -50,6 +56,25 @@ class CHABuilder implements CGBuilder<Invoke, JMethod> {
     private CallGraph<Invoke, JMethod> buildCallGraph(JMethod entry) {
         DefaultCallGraph callGraph = new DefaultCallGraph();
         callGraph.addEntryMethod(entry);
+
+        Queue<JMethod> worklist = new ArrayDeque<>();
+        worklist.add(entry);
+
+        while (!worklist.isEmpty()) {
+            JMethod method = worklist.poll();
+            if (callGraph.addReachableMethod(method)) {
+                for(Invoke callSite : callGraph.getCallSitesIn(method)) {
+                    CallKind callKind = CallGraphs.getCallKind(callSite);
+                    Set<JMethod> callees = resolve(callSite);
+                    for (JMethod callee: callees) {
+                        Edge<Invoke, JMethod> edge = new Edge<>(callKind, callSite, callee);
+                        callGraph.addEdge(edge);
+                        worklist.add(callee);
+                    }
+                }
+            }
+        }
+
         // TODO - finish me
         return callGraph;
     }
@@ -59,7 +84,48 @@ class CHABuilder implements CGBuilder<Invoke, JMethod> {
      */
     private Set<JMethod> resolve(Invoke callSite) {
         // TODO - finish me
-        return null;
+        Set<JMethod> T = new HashSet<>();
+        MethodRef methodRef = callSite.getMethodRef();
+        Subsignature subsignature = methodRef.getSubsignature();
+
+        CallKind callKind = CallGraphs.getCallKind(callSite);
+        if (callKind == CallKind.STATIC ) {
+            JClass static_class = methodRef.getDeclaringClass();
+            JMethod jMethod = static_class.getDeclaredMethod(subsignature);
+            if (jMethod != null) T.add(jMethod);
+        }
+        else if( callKind == CallKind.SPECIAL){
+            JClass m_class = methodRef.getDeclaringClass();
+            JMethod jMethod = dispatch(m_class, subsignature);
+            if (jMethod != null) T.add(jMethod);
+        }
+        else if (callKind == CallKind.VIRTUAL || callKind == CallKind.INTERFACE) {
+            //这里是有问题的，遍历mtype的子集和遍历receiverobject type的子集并作dispatch的结果是不能保证一样的？
+            JClass declared_receiver_class = methodRef.getDeclaringClass();
+            Queue<JClass> subClasses = new ArrayDeque<>();
+            //这个set主要是针对同级设计的
+            Set<JClass> visited = new HashSet<>();
+            subClasses.add(declared_receiver_class);
+
+            while(!subClasses.isEmpty()) {
+                JClass jClass = subClasses.poll();
+                JMethod jMethod = dispatch(jClass, subsignature);
+                if (jMethod != null) T.add(jMethod);
+
+                //需要考虑接口和实现吗，这个算法的本质是？
+                for (JClass sub : hierarchy.getDirectSubinterfacesOf(jClass)) {
+                    if (visited.add(sub)) subClasses.add(sub);
+                }
+                for (JClass sub : hierarchy.getDirectImplementorsOf(jClass)) {
+                    if (visited.add(sub)) subClasses.add(sub);
+                }
+                for (JClass sub : hierarchy.getDirectSubclassesOf(jClass)) {
+                    if (visited.add(sub)) subClasses.add(sub);
+                }
+            }
+
+        }
+        return T;
     }
 
     /**
@@ -70,6 +136,16 @@ class CHABuilder implements CGBuilder<Invoke, JMethod> {
      */
     private JMethod dispatch(JClass jclass, Subsignature subsignature) {
         // TODO - finish me
+        JMethod jMethod = jclass.getDeclaredMethod(subsignature);
+        if (jMethod != null && !jMethod.isAbstract()) {
+            return jMethod;
+        }
+        else {
+            JClass superClass = jclass.getSuperClass();
+            if (superClass != null) {
+                return dispatch(superClass, subsignature);
+            }
+        }
         return null;
     }
 }
